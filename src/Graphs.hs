2ccc9ab15f0aa511e89a0157f39a64f16a1fb7c8
Gustavo Bauke <baukegustavo@gmail.com>
2024-05-04 11:34:47 -0300

    WIP: type safe graph representation by adjency list

--- src/Graphs.hs
+++ src/Graphs.hs
@@ -16,6 +16,7 @@
 module Graphs where
 
 import Data.Kind
+import Data.Proxy
 import GHC.TypeLits
 
 type family Every (c :: k -> Constraint) (xs :: [k]) :: Constraint where
@@ -26,10 +27,10 @@ data HList xs where
   Nil :: HList '[]
   (:#) :: x -> HList xs -> HList (x ': xs)
 
-type CountNeighbors :: xs -> Nat
-type family CountNeighbors ns where
-  CountNeighbors Nil = 0
-  CountNeighbors (g :# gs) = 1 + CountNeighbors gs
+-- type CountNeighbors :: xs -> Nat
+-- type family CountNeighbors ns where
+--   CountNeighbors Nil = 0
+--   CountNeighbors (g :# gs) = 1 + CountNeighbors gs
 
 infixr 5 :#
 
@@ -37,25 +38,33 @@ instance (Every Show xs) => Show (HList xs) where
   show Nil = ""
   show (g :# gs) = show g ++ ", " ++ show gs
 
-type Graph :: Type -> Natural -> Type
-data Graph a n where
-  Graph :: a -> HList xs -> Graph a (CountNeighbors xs)
+-- type Graph :: Type -> Natural -> Type
+-- data Graph a n where
+--   Graph :: a -> HList xs -> Graph a (CountNeighbors xs)
 
-type family If (b :: Bool) (t :: k) (f :: k) :: k where
-  If 'True t f = t
-  If 'False t f = f
+-- type family If (b :: Bool) (t :: k) (f :: k) :: k where
+--   If 'True t f = t
+--   If 'False t f = f
 
-type family (==) (a :: k) (b :: k) :: Bool where
-  a == a = 'True
-  a == b = 'False
+-- type family (==) (a :: k) (b :: k) :: Bool where
+--   a == a = 'True
+--   a == b = 'False
 
-type family IsEven (n :: Nat) :: Constraint where
-  IsEven n = If (Mod n 2 == 0) (() :: Constraint) (TypeError ('Text "Expected an even number"))
+-- type family IsEven (n :: Nat) :: Constraint where
+--   IsEven n = If (Mod n 2 == 0) (() :: Constraint) (TypeError ('Text "Expected an even number"))
 
-type HasEulerianPath :: xs -> Constraint
-type family HasEulerianPath xs where
-  HasEulerianPath Nil = ()
-  HasEulerianPath (Graph a n :# gs) = (IsEven n, HasEulerianPath gs)
+-- type HasEulerianPath :: xs -> Constraint
+-- type family HasEulerianPath xs where
+--   HasEulerianPath Nil = ()
+--   HasEulerianPath (Graph a n :# gs) = (IsEven n, HasEulerianPath gs)
 
-type family HasEulerianPathBool xs where
-  HasEulerianPathBool xs = HasEulerianPath xs ~ (() :: Constraint)
+-- type family HasEulerianPathBool xs where
+--   HasEulerianPathBool xs = HasEulerianPath xs ~ (() :: Constraint)
+
+type Edge :: Symbol -> Symbol -> Type
+data Edge from to = Edge
+
+instance (KnownSymbol from, KnownSymbol to) => Show (Edge from to) where
+  show _ = symbolVal (Proxy @from) ++ " -> " ++ symbolVal (Proxy @to)
+
+edges = Edge @"a" @"b" :# Edge @"b" @"c" :# Edge @"c" @"a" :# Nil
